253b97c76e309382c3065a4e0853376bcb7e012b
chrisweidya <chrisweidya@hotmail.com>
2017-01-20 21:49:22 -0500

    first commit

---
+++ New Unity Project 1/Assets/Scripts/Startup.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Startup : MonoBehaviour {
+    public GameObject platform;
+    public int count = 0;
+    void Awake()
+    {
+        for(int i=0-count/2; i<count/2; i++)
+        {
+            Instantiate(platform, new Vector3(0.2f*i, 0, 0), Quaternion.identity);
+        }
+    }
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
