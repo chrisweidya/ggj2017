03de71da860b54bedb27043b11bc0b26c22370ff
chrisweidya <chrisweidya@hotmail.com>
2017-01-21 17:04:32 -0500

    Added start Scene

---
+++ New Unity Project 1/Assets/Scripts/StartButton.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartButton : MonoBehaviour {
+    Camera cam;
+	// Use this for initialization
+	void Start () {
+        cam = GetComponent<Camera>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit))
+            {
+                StartCoroutine(advanceToNextScene());
+            }
+        }
+    }
+
+    IEnumerator advanceToNextScene()
+    {
+        yield return new WaitForSeconds(2f);
+        SceneManager.LoadScene("Main2");
+    }
+}
